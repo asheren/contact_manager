13a63dcb745de7e6b2042a21edf1b690b2a21a46
Allison <asheren@neighborsations.com>
2013-08-09 13:44:46 -0400

    Make person spec more succint

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -5,7 +5,6 @@ let(:person) do
 	Person.new(first_name: 'Alice', last_name: 'Smith')
 end
   it 'is valid' do
-  	person = Person.new(first_name: 'Alice', last_name: 'Smith')
   	expect(person).to be_valid
   end
 
